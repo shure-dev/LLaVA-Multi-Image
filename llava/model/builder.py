fba41b129a8d655fd6ab26e76ded02839805531f
yusuke0919 <mkmysk123456789@gmail.com>
2023-11-06 15:52:37 +0900

    4bit

--- llava/model/builder.py
+++ llava/model/builder.py
@@ -23,7 +23,7 @@ from llava.model import *
 from llava.constants import DEFAULT_IMAGE_PATCH_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN
 
 
-def load_pretrained_model(model_path, model_base, model_name, load_8bit=True, load_4bit=False, device_map="auto", device="cuda"):
+def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, load_4bit=True, device_map="auto", device="cuda"):
     kwargs = {"device_map": device_map}
 
     if device != "cuda":
